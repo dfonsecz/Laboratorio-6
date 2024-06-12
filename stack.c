f4879867474c47bb281b7ab58b4e8d21ecb88c38
Danny Fonseca <daniela.fonsecazumbado@gmail.com>
2024-06-11 23:56:55 -0600

    Added stack.c file which defines a stack

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 100
+
+typedef struct {
+    int arr[MAX_SIZE];
+    int top; // Índice del elemento superior de la pila
+} Stack;
+
+int main() {
+    Stack s;
+}
