39c044a03950f0ef58345d07e976d2e5e9bb381f
Danny Fonseca <daniela.fonsecazumbado@gmail.com>
2024-06-12 00:16:36 -0600

    Modified stack.c to include function to push elemento to top part of the stack

--- stack.c
+++ stack.c
@@ -5,8 +5,25 @@
 
 typedef struct {
     int arr[MAX_SIZE];
-    int top; // Índice del elemento superior de la pila
+    int top; // índice del elemento superior de la pila
 } Stack;
+
+// Inicializar el stack
+void initialize(Stack *s) {
+    s->top = -1; // indica stack vacío
+}
+
+int isFull(Stack *s) {
+    return s->top == MAX_SIZE - 1;
+}
+
+void push(Stack *s, int value) {
+    if (isFull(s)){
+        printf("Stack Overflow: No se pueden agregar más elementos a la pila.\n");
+        return;
+    }
+    s->arr[++(s->top)] = value; // se incrementa el índice del elemento superior y luego se agrega el valor
+}
 
 int main() {
     Stack s;
