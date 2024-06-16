d5e09ad06bc7d45cc705d1f6912126d9ac525d9b
Danny Fonseca <daniela.fonsecazumbado@gmail.com>
2024-06-15 20:54:58 -0600

    Added more functions and documentation

--- stack.c
+++ stack.c
@@ -2,12 +2,28 @@
 #include <stdlib.h>
 #include "stack.h"
 
+/*
+Incluye las funciones usadas en main.c:
+initialize(): Inicializa la pila
+push(): Agrega un elemento a la parte superior de la pila
+pop(): Devuelve y elimina el elemento en la parte superior de la pila
+peek(): Devuelve el elemento en la parte superior de la pila sin eliminarlo
+isEmpty(): Verifica si la pila está vacía
+*/
+
 // Inicializar el stack
+/*
+@Stack: pila
+*/
 void initialize(Stack *s) {
     s->head = NULL; // indica stack vacío
 }
 
 // Agregar un elemento a la parte superior de la pila
+/*
+@Stack: pila
+@data: elemento a agregar a la pila
+*/
 void push(Stack *s, int data) {
     Node *newNode = (Node *)malloc(sizeof(Node));
     if (newNode == NULL) {
@@ -19,7 +35,29 @@ void push(Stack *s, int data) {
     s->head = newNode;
 }
 
+// Eliminar y devolver un elemento de la parte superior de la pila
+/*
+@Stack: pila
+*/
+int pop(Stack *s) {
+    if (isEmpty(s)) {
+        printf("La pila está vacía\n");
+        return -1; // Error
+    }
+
+    Node *temp = s->head; // Guarda el nodo que está en la cabeza
+    int data = temp->data; // Guarda la información que se va a retornar
+    s->head = s->head->next; // Ajusta el head para que apunte al nodo siguiente
+    free(temp); // Libera la memoria del nodo eliminado
+
+    return data;
+}
+
 // Devuelve el elemento en una parte dada de la pila sin eliminarlo
+/*
+@Stack: pila
+@position: posición del elemento a la que se hará peek
+*/
 int peek(Stack *s, int position) {
     Node *pointer = s->head;
     for (int i = 0; i < position - 1 && pointer != NULL; i++) {
@@ -33,6 +71,9 @@ int peek(Stack *s, int position) {
 }
 
 // Revisa si la pila está vacía
+/*
+@Stack: pila
+*/
 int isEmpty(Stack *s) {
     return s->head == NULL;
 }
